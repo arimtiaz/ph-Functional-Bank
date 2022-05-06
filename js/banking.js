89aac3e24c58bcc0ebe4c74ac0bb66cf7487b788
arimtiaz <abadur391@gmail.com>
2022-05-06 15:15:21 +0600

    Done

--- js/banking.js
+++ js/banking.js
@@ -1,10 +1,11 @@
 document.getElementById('deposit-button').addEventListener('click', function(){
     const depositInput = document.getElementById('deposit-input');
     const depositAmount = depositInput.value;
-    console.log(depositAmount);
 
     // get current deposit
     const depositTotal = document.getElementById('total-deposit');
-    const depositTotalText = depositTotal.innerText;
-    console.log(depositTotalText);
+    depositTotal.innerText = depositAmount;
+
+    // Clear Input Field 
+    depositInput.value = ' ';
 })
